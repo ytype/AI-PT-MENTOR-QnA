3b8bad11cedad7fe3dd66374bd3ffeaa38e093e3
ytype <ytype33@gmail.com>
2020-09-25 12:23:32 +0900

    feat: add axios example, doc: update readme

---
+++ http-axios/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default Vue.extend({
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    fetchData() {
+      axios.get('localhost:5000')
+        .then((response) => {
+          console.log(response)
+        })
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
